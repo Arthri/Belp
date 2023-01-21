a628e832a6d900351678489b433222d6a7e64629
Arthri <41360489+a@users.noreply.github.com>
2023-01-21 12:05:26 +0000

    Add basic tests

---
+++ tests/Belp.CodeAnalysis.ManifestResourceGenerators.UnitTests/CSharpIncrementalSourceGeneratorVerifier.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Testing;
+using Microsoft.CodeAnalysis.Testing;
+using Microsoft.CodeAnalysis.Testing.Verifiers;
+
+namespace Belp.CodeAnalysis.ManifestResourceGenerators.UnitTests;
+
+internal class CSharpIncrementalSourceGeneratorVerifier<TSourceGenerator>
+    where TSourceGenerator : IIncrementalGenerator, new()
+{
+    public class Test : CSharpSourceGeneratorTest<EmptySourceGeneratorProvider, XUnitVerifier>
+    {
+        public LanguageVersion LanguageVersion { get; init; } = LanguageVersion.CSharp7_3;
+
+        /// <inheritdoc />
+        protected override IEnumerable<ISourceGenerator> GetSourceGenerators()
+        {
+            return new TSourceGenerator().AsSourceGenerator().AsSingletonEnumerable();
+        }
+
+        /// <inheritdoc />
+        protected override ParseOptions CreateParseOptions()
+        {
+            return ((CSharpParseOptions)base.CreateParseOptions()).WithLanguageVersion(LanguageVersion);
+        }
+    }
+}
---
+++ tests/Belp.CodeAnalysis.ManifestResourceGenerators.UnitTests/ManifestResourcesGeneratorTests/DiagnosticsTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.CodeAnalysis.Testing;
+using SGTest = Belp.CodeAnalysis.ManifestResourceGenerators.UnitTests.CSharpIncrementalSourceGeneratorVerifier<Belp.CodeAnalysis.ManifestResourceGenerators.ManifestResourcesGenerator>.Test;
+
+namespace Belp.CodeAnalysis.ManifestResourceGenerators.UnitTests.ManifestResourcesGeneratorTests;
+
+public class DiagnosticsTest
+{
+    private class EmptyProjectNameTest : SGTest
+    {
+        protected override string DefaultTestProjectName => "";
+    }
+
+    [Fact]
+    public async Task AssemblyName_eq_null_Errors()
+    {
+        var test = new EmptyProjectNameTest
+        {
+            ReferenceAssemblies = ReferenceAssemblies.NetStandard.NetStandard20,
+            ExpectedDiagnostics =
+            {
+                DiagnosticResult.CompilerError("CS8203"),
+                new DiagnosticResult(DiagnosticDescriptors.SourceGenerators.MRG4001),
+            },
+        };
+
+        await test.RunAsync();
+    }
+}
