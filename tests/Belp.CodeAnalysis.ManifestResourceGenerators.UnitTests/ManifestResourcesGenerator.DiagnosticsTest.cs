8e14d99fe3ffe5f0a54a2dd73963c9fb2df874dd
Arthri <41360489+a@users.noreply.github.com>
2023-01-21 15:28:26 +0000

    Add test for MRGN4002

--- tests/Belp.CodeAnalysis.ManifestResourceGenerators.UnitTests/ManifestResourcesGenerator.DiagnosticsTest.cs
+++ tests/Belp.CodeAnalysis.ManifestResourceGenerators.UnitTests/ManifestResourcesGenerator.DiagnosticsTest.cs
@@ -1,4 +1,4 @@
-using Microsoft.CodeAnalysis.Testing;
+﻿using Microsoft.CodeAnalysis.Testing;
 using SGTest = Belp.CodeAnalysis.ManifestResourceGenerators.UnitTests.CSharpIncrementalSourceGeneratorVerifier<Belp.CodeAnalysis.ManifestResourceGenerators.ManifestResourcesGenerator>.Test;
 
 namespace Belp.CodeAnalysis.ManifestResourceGenerators.UnitTests;
@@ -25,6 +25,42 @@ public partial class ManifestResourcesGenerator
                     {
                         DiagnosticResult.CompilerError("CS8203"),
                         new DiagnosticResult(DiagnosticDescriptors.SourceGenerators.MRG4001),
+                    },
+                },
+            };
+
+            return test.RunAsync();
+        }
+
+        [Fact]
+        public Task When_ResourceName_ends_with_X22X2EX22X2C_then_expect_MRGN4002()
+        {
+            var test = new SGTest
+            {
+                ReferenceAssemblies = ReferenceAssemblies.NetStandard.NetStandard20,
+
+                TestState =
+                {
+                    AdditionalFiles =
+                    {
+                        ("/File.txt", ""),
+                    },
+                    AnalyzerConfigFiles =
+                    {
+                        ("/.editorconfig", """
+                        [/File.txt]
+                        build_metadata.AdditionalFiles.ManifestResourceName = File.txt.
+                        build_metadata.AdditionalFiles.TargetSourceGenerator = ManifestResourcesGenerator
+                        """),
+                    },
+
+                    ExpectedDiagnostics =
+                    {
+                        new DiagnosticResult(DiagnosticDescriptors.SourceGenerators.ManifestResourcesGenerator.MRGN4002),
+                    },
+                    GeneratedSources =
+                    {
+                        Source_ManifestResourcesHelper,
                     },
                 },
             };
