2a159e26d9323845b55b1bd1395b9e979292c7c4
Arthri <41360489+a@users.noreply.github.com>
2023-01-21 15:18:44 +0000

    Add property for source ManifestResourcesHelper

--- tests/Belp.CodeAnalysis.ManifestResourceGenerators.UnitTests/ManifestResourcesGenerator.cs
+++ tests/Belp.CodeAnalysis.ManifestResourceGenerators.UnitTests/ManifestResourcesGenerator.cs
@@ -1,5 +1,12 @@
-﻿namespace Belp.CodeAnalysis.ManifestResourceGenerators.UnitTests;
+﻿using Microsoft.CodeAnalysis.Text;
+using System.Text;
+
+namespace Belp.CodeAnalysis.ManifestResourceGenerators.UnitTests;
 
 public static partial class ManifestResourcesGenerator
 {
+    public static (string filename, SourceText content) Source_ManifestResourcesHelper => (
+        """Belp.CodeAnalysis.ManifestResourceGenerators\Belp.CodeAnalysis.ManifestResourceGenerators.ManifestResourcesGenerator\ManifestResourcesHelper.cs""",
+        SourceText.From(ManifestResourceGenerators.ManifestResourcesGenerator.Code_ManifestResourcesHelper, Encoding.UTF8)
+    );
 }
