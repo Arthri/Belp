e5dade8dde5b97a57a65e5f0cb1f29ae880701dc
Arthri <41360489+a@users.noreply.github.com>
2023-01-22 13:51:10 +0000

    Add generation test for ManifestResourcesGenerator

---
+++ tests/Belp.CodeAnalysis.ManifestResourceGenerators.UnitTests/Resources.cs
@@ -0,0 +1,11 @@
+﻿using IOPath = System.IO.Path;
+
+namespace Belp.CodeAnalysis.ManifestResourceGenerators.UnitTests;
+
+internal static partial class Resources
+{
+    public static string Path { get; } = IOPath.Combine(
+        IOPath.GetDirectoryName(typeof(Resources).Assembly.Location) ?? "/",
+        "Resources"
+    );
+}
