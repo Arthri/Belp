d29ac98e26037b0a9b0cd57b3e7990dbd5e800e3
Arthri <41360489+a@users.noreply.github.com>
2023-02-23 16:59:23 +0000

    Add Initializer for MSBuildLocator

---
+++ src/internals/Belp.Tests.MSBuild.xUnit/Initializer.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Build.Locator;
+using System.Runtime.CompilerServices;
+
+file static class Initializer
+{
+    [ModuleInitializer]
+    public static void InitializeModule()
+    {
+        if (MSBuildLocator.IsRegistered)
+        {
+            return;
+        }
+
+        VisualStudioInstance? latestInstance = MSBuildLocator
+            .QueryVisualStudioInstances()
+            .Where(static i => i.DiscoveryType == DiscoveryType.DotNetSdk)
+            .OrderByDescending(static i => i.Version)
+            .FirstOrDefault()
+            ;
+
+        if (latestInstance is null)
+        {
+            throw new InvalidOperationException(".NET SDK not found");
+        }
+
+        if (latestInstance.Version.Major < 5)
+        {
+            throw new InvalidOperationException(".NET SDK 5 or higher not found");
+        }
+
+        MSBuildLocator.RegisterInstance(latestInstance);
+    }
+}
