1abcf72842b8ff92b7ad0e89bbdc6446f0023633
Arthri <41360489+a@users.noreply.github.com>
2023-02-23 18:20:48 +0000

    Add example msbuild tests

--- tests/Belp.Build.Autoasmver.IntegrationTests/AssemblyVersionTests.cs
+++ tests/Belp.Build.Autoasmver.IntegrationTests/AssemblyVersionTests.cs
@@ -1,11 +1,18 @@
-﻿using System.Xml;
+﻿namespace Belp.Build.Autoasmver.IntegrationTests;
 
-namespace Belp.Build.Autoasmver.IntegrationTests;
-
-public class AssemblyVersionTests
+public class AssemblyVersionTests : MSBuildTest
 {
+    public AssemblyVersionTests(ITestOutputHelper output) : base(output)
+    {
+    }
+
     [Fact]
     public void Expect_AssemblyVersion_Set()
     {
+        var result = RequestBuild(Path.Combine(Resources.Path, "AssemblyVersionTests", "Test.Project1", "Test.Project1.csproj"));
+
+        ProjectPropertyInstance p_AssemblyVersion = result.ProjectStateAfterBuild.GetProperty("AssemblyVersion");
+        p_AssemblyVersion.Should().NotBeNull();
+        p_AssemblyVersion.EvaluatedValue.Should().Be("5.0.0.0");
     }
 }
