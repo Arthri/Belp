563f0a90edcf59b2030f0ba310183a654d9e9391
Arthri <41360489+a@users.noreply.github.com>
2023-02-23 17:50:36 +0000

    Add singleton enumerable to MSBuild.xUnit

---
+++ src/internals/Belp.Tests.MSBuild.xUnit/Extensions/AllExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Belp.Tests.MSBuild.xUnit.Extensions;
+
+/// <summary>
+/// Provides extension methods for all types.
+/// </summary>
+internal static class AllExtensions
+{
+    /// <summary>
+    /// Wraps the specified element in a singleton enumerable.
+    /// </summary>
+    /// <typeparam name="T">The type of objects to enumerate.</typeparam>
+    /// <param name="element">The element to iterate over.</param>
+    /// <returns>A singleton enumerable iterating over the specified member.</returns>
+    public static SingletonEnumerable<T> AsSingletonEnumerable<T>(this T element)
+    {
+        return new SingletonEnumerable<T>(element);
+    }
+}
