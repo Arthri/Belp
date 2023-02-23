1da7ef254ae962781a7bba786d0119dc1327fb75
Arthri <41360489+a@users.noreply.github.com>
2023-02-23 18:13:42 +0000

    Add Resources.BasePath for Resources folder's parent path

--- src/internals/Belp.Tests.Common/Resources.cs
+++ src/internals/Belp.Tests.Common/Resources.cs
@@ -1,5 +1,6 @@
-﻿namespace Belp.Tests.Common;
-using IOPath = System.IO.Path;
+﻿using IOPath = System.IO.Path;
+
+namespace Belp.Tests.Common;
 
 /// <summary>
 /// Provides methods for reading or writing resources for the running.
@@ -7,10 +8,15 @@ using IOPath = System.IO.Path;
 public static partial class Resources
 {
     /// <summary>
+    /// Gets the path where the resources folder is located.
+    /// </summary>
+    public static string BasePath { get; } = IOPath.GetDirectoryName(typeof(Resources).Assembly.Location) ?? Environment.CurrentDirectory;
+
+    /// <summary>
     /// Gets the base resources path.
     /// </summary>
     public static string Path { get; } = IOPath.Combine(
-        IOPath.GetDirectoryName(typeof(Resources).Assembly.Location) ?? Environment.CurrentDirectory,
+        BasePath,
         "Resources"
     );
 }
