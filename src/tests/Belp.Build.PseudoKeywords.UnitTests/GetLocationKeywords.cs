7dcd9e4ae09ee6a83133a36373e2224aa48de779
Arthri <41360489+a@users.noreply.github.com>
2023-04-03 13:13:23 +0000

    Add types for get location

---
+++ src/tests/Belp.Build.PseudoKeywords.UnitTests/GetLocationKeywords.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Belp.Build.PseudoKeywords.UnitTests;
+
+public partial class GetLocationKeywords
+{
+    [Fact]
+    public void linenum_is_420()
+    {
+#line 420
+        linenum().Should().Be(420);
+    }
+
+    [Fact]
+    public void currentfile_is_FooX2Ecs()
+    {
+#line 420 "Foo.cs"
+        currentfile().Should().BeEmpty().And.Be(GetFilePath());
+
+        static string GetFilePath([CallerFilePath] string path = "")
+        {
+            return path;
+        }
+    }
+}
