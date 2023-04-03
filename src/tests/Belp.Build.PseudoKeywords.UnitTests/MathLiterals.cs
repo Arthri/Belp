1740302dfc4d68f5f021337d49eb9a2b248e5d06
Arthri <41360489+a@users.noreply.github.com>
2023-04-03 13:11:00 +0000

    Add tests for math literals

---
+++ src/tests/Belp.Build.PseudoKeywords.UnitTests/MathLiterals.cs
@@ -0,0 +1,22 @@
+﻿namespace Belp.Build.PseudoKeywords.UnitTests;
+
+public partial class MathLiterals
+{
+    [Fact]
+    public void φ_eq_1X2E6180339887498948()
+    {
+        φ.Should().Be(1.6180339887498948);
+    }
+
+    [Fact]
+    public void π_eq_MathX2EPI()
+    {
+        π.Should().Be(Math.PI);
+    }
+
+    [Fact]
+    public void τ_eq_MathX2ETau()
+    {
+        τ.Should().Be(Math.Tau);
+    }
+}
